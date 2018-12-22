007f6213102b22d5d22846e8e80a6b512d4b6b06
Roger Ko <rogerko@Rogers-MacBook-Pro.local>
2018-12-22 15:08:20 +0800

    Allow ZDT option.

--- benchmark_calc.py
+++ benchmark_calc.py
@@ -7,14 +7,21 @@ from utils.utils import save_values
 n = 10
 dim = 6
 fdim = 2
+problem_name = 'dtlz'
+problem_number = 7
 
 # python benchmark_calc.py 10 6 2
 if (len(sys.argv) > 0):
     n = int(sys.argv[1])
     dim = int(sys.argv[2])
     fdim = int(sys.argv[3])
+    problem_name = (sys.argv[4])
+    if (problem_name != 'dtlz' and problem_name != 'zdt'):
+        raise Exception('Please choose between DTLZ or ZDT')
+    if (problem_name == 'zdt'):
+        problem_number = 6
 
-max_fevals = (dim+1) * 20
+max_fevals = (dim+1) * 2
 
 # To account for the fact that the zero index array in util functions
 # are actually the 1st feval
@@ -22,17 +29,20 @@ working_fevals = max_fevals-1
 pop_size = 24
 seed = 33
 
-# For the 7 dtlz problems
-for i in range(7):
-    problem = pg.problem(pg.problems.dtlz(i+1, dim=dim, fdim=fdim))
-    # problem = pg.problem(pg.problems.dtlz(2, dim=6, fdim=2))
+# For the each problem in the problem suite
+for i in range(problem_number):
+    problem_function = getattr(pg.problems, problem_name)
+    if (problem_name == "dtlz"):
+        problem = pg.problem(problem_function(i+1, dim=dim, fdim=fdim))
+    else:
+        problem = pg.problem(problem_function(i+1, param=dim))
     algo_moead = pg.algorithm(pg.moead(gen=1))
     algo_nsga2 = pg.algorithm(pg.nsga2(gen=1))
 
     # Hypervolume calculations, mean taken over n number of times
-    hv_rbfmopt_plot = calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem, (i+1))
-    hv_moead_plot = calculate_mean_pyg(n, algo_moead, working_fevals, pop_size, seed, problem, (i+1))
-    hv_nsga2_plot = calculate_mean_pyg(n, algo_nsga2, working_fevals, pop_size, seed, problem, (i+1))
+    hv_rbfmopt_plot = calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem)
+    hv_moead_plot = calculate_mean_pyg(n, algo_moead, working_fevals, pop_size, seed, problem)
+    hv_nsga2_plot = calculate_mean_pyg(n, algo_nsga2, working_fevals, pop_size, seed, problem)
     fevals_plot = range(0, max_fevals)
 
     save_values('storedvalues/rbfmopt_hv_dtlz' + str(i+1) + '_fevals' + str(max_fevals) + '.txt', hv_rbfmopt_plot.tolist())
--- utils/pygmo_utils.py
+++ utils/pygmo_utils.py
@@ -39,7 +39,7 @@ def reconstruct_hv_per_feval_meta(max_fevals, x_list, f_list, hv_pop):
 # Only for the evolutionary algos
 # Stores the f and x values for each generation of the evolutionary algo,
 # Then calculate the hypervolume per function evaluation
-def get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, nproblem, run):
+def get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, run):
 
     max_gen = math.ceil(max_fevals/pop_size)
 
@@ -55,8 +55,8 @@ def get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, nproblem, run):
         f_list = numpy.concatenate((f_list, pop.get_f()))
         x_list = numpy.concatenate((x_list, pop.get_x()))
 
-    save_values('storedvalues/' + algo.get_name().split(':')[0] + '_x_dtlz' + str(nproblem) + '_run' + str(run) + '.txt', x_list.tolist())
-    save_values('storedvalues/' + algo.get_name().split(':')[0] + '_f_dtlz' + str(nproblem) + '_run' + str(run) + '.txt', f_list.tolist())
+    save_values('storedvalues/' + algo.get_name().split(':')[0] + '_x_' + problem.get_name() + '_run' + str(run) + '.txt', x_list.tolist())
+    save_values('storedvalues/' + algo.get_name().split(':')[0] + '_f_' + problem.get_name() + '_run' + str(run) + '.txt', f_list.tolist())
 
     pop_empty = pg.population(prob=problem, seed=seed)
 
@@ -66,19 +66,18 @@ def get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, nproblem, run):
 # Meta-heuristic algorithms are stochastic and need to be run many times.
 # Calculates the hypervolume n times, and then gets the mean across columns
 # to give a 1D mean array
-def calculate_mean_pyg(n, algo, max_fevals, pop_size, seed, problem, nproblem):
+def calculate_mean_pyg(n, algo, max_fevals, pop_size, seed, problem):
 
     # 2D array whose elements are the n arrays of hypervolume
     return_array = []
     for i in range(n):
-        return_array.append(get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, nproblem, (i+1)))
+        return_array.append(get_hv_for_algo(algo, max_fevals, pop_size, seed, problem, (i+1)))
         # Make sure we change the seed each time the algo is being run
         seed += (i+1)
     return numpy.mean(return_array, axis=0)
 
 
-# nproblem is the dtlz number
-def calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem, nproblem):
+def calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem):
     return_array = []
 
     for i in range(n):
@@ -99,8 +98,8 @@ def calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem, nproblem):
         x_list = np.array(algo_rbfmopt.get_x_list())
         f_list = np.array(algo_rbfmopt.get_f_list())
 
-        save_values('storedvalues/rbfmopt_x_dtlz' + str(nproblem) + '_run' + str(i+1) + '.txt', x_list.tolist())
-        save_values('storedvalues/rbfmopt_f_dtlz' + str(nproblem) + '_run' + str(i+1) + '.txt', f_list.tolist())
+        save_values('storedvalues/rbfmopt_x_' + problem.get_name() + '_run' + str(i+1) + '.txt', x_list.tolist())
+        save_values('storedvalues/rbfmopt_f' + problem.get_name() + '_run' + str(i+1) + '.txt', f_list.tolist())
 
         return_array.append(reconstruct_hv_per_feval(working_fevals, algo_rbfmopt.get_x_list(), algo_rbfmopt.get_f_list(), empty_pop))
 
