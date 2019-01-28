fa9e4216ef15d1a7ca3b2a3193870364fc321e14
Roger Ko <rogerko@Rogers-MacBook-Pro.local>
2019-01-28 20:49:30 +0800

    pygmoudp

--- comparing_pygmo_dtlz4.py
+++ comparing_pygmo_dtlz4.py
@@ -2,7 +2,9 @@ import sys
 import pygmo as pg
 from utils.pygmo_utils import calculate_mean_rbf
 from utils.utils import save_values
-# from problem.dtlz4 import dtlz4
+from problem.dtlz4 import dtlz4
+from rbfmopt.PygmoUDP import PygmoUDP
+import numpy as np
 
 # where n is the number of times the meta-heuristic algorithms are run to get the mean
 n = 1
@@ -21,9 +23,13 @@ pop_size = 24
 seed = 33
 
 # For the each problem in the problem suite
-problem_function = getattr(pg.problems, problem_name)
+# problem_function = getattr(pg.problems, problem_name)
 
-problem = pg.problem(problem_function(problem_number, dim=dim, fdim=fdim))
+# problem = pg.problem(problem_function(problem_number, dim=dim, fdim=fdim))
+
+udp = PygmoUDP(dim, np.array([-10] * dim), np.array([10] * dim), ['R'], fdim, dtlz4)
+
+problem = pg.problem(udp)
 
 # Hypervolume calculations, mean taken over n number of times
 hv_rbfmopt_plot = calculate_mean_rbf(n, max_fevals, working_fevals, seed, problem, cycle)
